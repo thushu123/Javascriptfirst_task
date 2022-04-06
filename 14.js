3135a5be28f4e987d0c0c468ab6317cc91f53bdf
Thushara <thushara1994@gmail.com>
2022-04-06 23:41:41 +0530

    changed 14.js

--- 14.js
+++ 14.js
@@ -5,20 +5,35 @@ for(let i=2; i <=20; i++){
 // console.log("option",option);
 document.getElementById("num").innerHTML = options;
 multiplication =() =>{
-   let result= "" ;
-   //to get values seperately
-  //  let result = "" ;
-   //get value of selected number
-  //  let a= document.getElementById("number");
-  var num = document.getElementById('num').value;
-  // let b= num.options[num.delectedIndex].value;
-  console.log('selectednum,',num);
-  for (let i = 1 ; i<=10 ; i++){
-    result += i * num  ; 
+  //  let result= "" ;
+  //  //to get values seperately
+  // //  let result = "" ;
+  //  //get value of selected number
+  // var num = document.getElementById('num').value;
+  // console.log('selectednum,',num);
+  // for (let i = 1 ; i<=10 ; i++){
+  //   result += i * num  ; 
+  //   var r = result.split(/(\s+)/);
     
-  }
-  document.writeln("<result>"+result + "<result>");
+  // }
+  // document.writeln("<br>"+r );
+  // //let num="";
+            let result = "";
+            //var value = select.options[select.selectedIndex].value;
+            let a=document.getElementById("num");
+            let b=a.options[a.selectedIndex].value;
+        
+            console.log(b);
+            for(let i=1;i<=10;i++)
+            {
+                result=i*b;
+                //document.getElementById("num1").value=result;
+                document.writeln( result+"<br>");
+                console.log(result);
+            }
+           // document.getElementById("demo").innerHTML=num;
+        }
 
-}
+
    
  
